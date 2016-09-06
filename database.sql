f4aaa756903d544c41fda1d565f5d7c49b4cc278
jca248 <jca248@sfu.ca>
2016-09-06 01:17:53 +0000

    Initial repo

---
+++ database.sql
@@ -0,0 +1,26 @@
+CREATE TABLE user(
+    username VARCHAR(12),
+	school VARCHAR(50),
+	course CHAR(12),
+    PRIMARY KEY (username)) ENGINE MyISAM;
+
+
+CREATE TABLE book(
+	name VARCHAR(50),
+	id INT NOT NULL AUTO_INCREMENT,
+    edition INT,
+    quality INT,
+    price DECIMAL(13,2),
+    usefulness INT,
+    comments VARCHAR(350),
+    PRIMARY KEY (name,id)) ENGINE MyISAM;
+
+
+CREATE TABLE describe (
+	username VARCHAR(12),
+	name VARCHAR(50),
+    PRIMARY KEY (username,name),
+    FOREIGN KEY (username)
+         REFERENCES user,
+    FOREIGN KEY (name)
+         REFERENCES book) ENGINE MyISAM;
